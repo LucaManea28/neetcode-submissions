15cad0857c41e32765254a9c5787903d6a3ce3ba
Manea Luca <lucamanea5@gmail.com>
2026-09-01 12:20:18 +0300

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '(' || c == '[' || c == '{'){
+                stack.push(c);
+            }else{ 
+                if(stack.isEmpty())
+                    return false;
+                if(c == ')' && stack.peek() != '(') return false;
+                if(c == ']' && stack.peek() != '[') return false;
+                if(c == '}' && stack.peek() != '{') return false;
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
