08124210faf88d0a2af9a11437e53563691b3ef1
Manea Luca <lucamanea5@gmail.com>
2026-09-01 12:53:14 +0300

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        for(int i=0; i<temperatures.length; i++){
+            int j = i+1;
+            while(j<temperatures.length && temperatures[i] >= temperatures[j])
+                j++;
+            if(j < temperatures.length)
+                result[i] = j - i;
+        }
+        return result;
+    }
+}
