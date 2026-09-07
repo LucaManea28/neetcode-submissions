fc12c344bd97ba86404da4bc7e428e882bd8eac3
Manea Luca <lucamanea5@gmail.com>
2026-09-07 14:41:19 +0300

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length == 1)
+            return nums[0];
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        sum[1] = Math.max(nums[0], nums[1]);
+        for(int i=2; i<nums.length; i++){
+            sum[i] = Math.max(nums[i] + sum[i-2], sum[i-1]);
+        }
+        return sum[nums.length - 1];
+    }
+}
