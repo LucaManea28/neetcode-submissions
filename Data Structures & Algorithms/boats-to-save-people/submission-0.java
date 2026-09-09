2859fa39b55f2dbf22d59919413cbba1afe6464b
Manea Luca <lucamanea5@gmail.com>
2026-09-09 15:14:20 +0300

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int boats = 0;
+        Arrays.sort(people);
+        int left = 0, right = people.length - 1;
+        while(left <= right){
+            if(people[left] + people[right] <= limit){
+                boats++;
+                left++;
+                right--;
+            }else{
+                boats++;
+                right--;
+            }
+        }
+        return boats;
+    }
+}
