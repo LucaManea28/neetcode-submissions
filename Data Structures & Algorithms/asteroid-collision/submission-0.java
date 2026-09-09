9613e292245b0f285c6cb5db553ac1ae837e7550
Manea Luca <lucamanea5@gmail.com>
2026-09-09 17:33:43 +0300

    Add: asteroid-collision - submission-0

---
+++ Data Structures & Algorithms/asteroid-collision/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i=0; i<asteroids.length; i++){
+            if(asteroids[i] > 0){
+                stack.push(asteroids[i]);
+            }else{
+                while(!stack.isEmpty() && stack.peek() > 0 && stack.peek() < Math.abs(asteroids[i]))
+                    stack.pop();
+                if(!stack.isEmpty() && stack.peek() == Math.abs(asteroids[i]))
+                    stack.pop();
+                else if(stack.isEmpty() || stack.peek() < 0)
+                    stack.push(asteroids[i]);
+            }
+        }
+        int[] result = new int[stack.size()];
+        for(int i = result.length - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+        
+        return result;
+    }
+}
